1e9d386fd0d6bd0d3480a449aa7019060c219e56
Nick Worthington <nickrworthington@gmail.com>
2026-06-30 08:36:18 +1000

    operate function computes one of the four operations on two operands

--- script.js
+++ script.js
@@ -14,6 +14,30 @@ function divide(num1, num2) {
     if (num2 != 0) {
         return num1 / num2;
     } else {
-        return `ERROR`;
+        return `ERROR: divide()`;
+    }
+}
+
+const ADD = 0;
+const SUB = 1;
+const MUL = 2;
+const DIV = 3;
+
+let operator = 0;
+let operand1 = 0;
+let operand2 = 0;
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case ADD:
+            return add(num1, num2);
+        case SUB:
+            return subtract(num1, num2);
+        case MUL:
+            return multiply(num1, num2);
+        case DIV:
+            return divide(num1, num2);
+        default:
+            return console.log(`ERROR: operate()`);
     }
 }
